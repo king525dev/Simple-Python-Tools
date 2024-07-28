81f41ac6ce343674865958e83c7f71efa0f4b66f
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2024-07-28 01:28:13 +0100

    Update pdfMerger.py

--- pdfMerger/code/pdfMerger.py
+++ pdfMerger/code/pdfMerger.py
@@ -2,9 +2,20 @@ import sys
 import os
 from PyPDF2 import PdfMerger, PdfReader
 
+print("""
+=================================
+     PDF MERGER
+=================================
+""")
+
 # Create a PdfFileMerger
 merger = PdfMerger()
 
+print("This program will merge a copy of every PDF document in this directory and save it to a single PDF")
+name = input("Name the output file: ") or "document-output"
+start = input("Press enter to start...\n")
+
+print("=====")
 # Append pages from PDF files
 for file in os.listdir(os.curdir):
      if file.endswith(".pdf"):
@@ -12,4 +23,9 @@ for file in os.listdir(os.curdir):
           merger.append(PdfReader(file))
 
 # Write the merged output to a new file
-merger.write("document-output.pdf")
+merger.write(f"{name}.pdf")
+
+print("=====")
+
+print("\nFiles Successfully Merged")
+end = input("Press enter to close...")
