2bd4bd66a902d44a4ef1d363153cdb7d046c2b39
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2024-07-28 01:01:56 +0100

    Update qrCodeGenerator.py

--- qrCodeGenerator/qrCodeGenerator.py
+++ qrCodeGenerator/qrCodeGenerator.py
@@ -18,7 +18,30 @@ def main():
           qrCode.save(
                f"{name}.png",
                scale=10
-               )
+               );
+          print("\nQR Code Successfully Generated")
+          end = input("Press enter to close....")
+          return;
+     
+     bgColour = input("\nInput your desired background colour: ") or "white"
+     fgColour = input("Input your desired foreground colour: ") or "black"
+     size = int(input("Input your desired QR code size: ") or "10")
+     borderSize = int(input("Input your desired QR code border size: ") or "5") 
+     borderColour = input("Input your desired QR code border colour: ") or "white"
+     fileFormat = input("What format do you want to save you QR code in?: ") or "png"
+
+     qrCode = segno.make_qr(link)
+     qrCode.save(
+          f"{name}.{fileFormat}",
+          scale=size,
+          light=bgColour,
+          dark=fgColour,
+          quiet_zone=borderColour,
+          border=borderSize,
+          );
+     print("\nQR Code Successfully Generated")
+     end = input("Press enter to close....")
+     return;
 
 main()
 
