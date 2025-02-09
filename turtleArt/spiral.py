3ccce5417404238b21b4381f146050980f34cd62
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2025-02-09 12:20:28 +0000

    Turtle art

---
+++ turtleArt/spiral.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+bgcolor("black")
+speed(0)
+hideturtle()
+
+for i in range(120):
+     color("red")
+     circle(i)
+     color("orange")
+     circle(i*0.8)
+     right(3)
+     forward(3)
+
+for i in range(120):
+     color("green")
+     circle(i)
+     color("lime")
+     circle(i*0.8)
+     left(3)
+     backward(3)
+
+done();
