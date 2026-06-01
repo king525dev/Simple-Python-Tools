455d12f69558de0ba437e26cf0a7df694bb75589
Ore Ajibade <109004601+king525dev@users.noreply.github.com>
2026-06-01 11:05:54 +0100

    Started on file organiser

---
+++ fileOrganiser/main.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+FILE_STRUCTURE = {
+     "Images": [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff"],
+     "Videos": [".mp4", ".mkv", ".flv", ".avi", ".mov"],
+     "Documents": [".pdf", ".doc", ".docx", ".txt", ".ppt", ".pptx", ".xls", ".xlsx"],
+     "Audio": [".mp3", ".wav", ".aac", ".flac"],
+     "Archives": [".zip", ".rar", ".7z", ".tar", ".gz"],
+     "Data": [".csv", ".json", ".xml"],
+     "Others": []
+}
+
